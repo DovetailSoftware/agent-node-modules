606928676b57b4b336164789f9bab2503225afcf
Craig Jennings <craig.jennings11@gmail.com>
2016-04-19 15:14:27 -0500

    sealing package.json dependencies of version 9.0.2, using npm 3.8.3

---
+++ iedriver/lib/iedriver.js
@@ -0,0 +1,14 @@
+var path = require('path');
+exports.path = process.platform === 'win32' ? path.join(__dirname, 'iedriver', 'IEDriverServer.exe') : path.join(__dirname, 'iedriver', 'iedriver');
+exports.binaryversion = '2.53.0'
+exports.version = '2.53';
+exports.start = function() {
+  exports.defaultInstance = require('child_process').execFile(exports.path);
+  return exports.defaultInstance;
+};
+exports.stop = function () {
+  if (exports.defaultInstance !== null){
+    exports.defaultInstance.kill();
+  }
+};
+exports.md5 = '63066830741d59ae6c74045a9f24291c';
